328fb819b28c4b8d85970c9ee3032448d12efd3f
Maxx Rodriguez <Maxx.Rodriguez@microchip.com>
2020-08-28 12:51:47 -0700

    added some basic web formatting

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("Hello from Firebase!");
+    // res.send("Hello from Firebase!");
+    res.render("index")
 })
 
 module.exports = router;
